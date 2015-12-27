6b48015cbc2110dc8d04086fee00062cecf756f8
Stéphane Derosiaux <derste@gmail.com>
2015-12-27 01:51:54 +0100

    Add jsx syntax

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@
 var App = React.createClass({
   render: function() {
     var items = this.props.subjects.map(function(subject) {
-      return React.createElement("li", null, subject);
+      return <li>{subject}</li>;
     });
-    return React.createElement("ul", null, "You like ", items);
+    return <ul>You like {items}</ul>;
   }
 });
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(React.createElement(App, { subjects: [ "Javascript", "Java" ] }), mountNode);
+ReactDOM.render(<App subjects={[ "Javascript", "Java" ]} />, mountNode);
