0ea00808961337b027854a77a83b903bd4a0d854
Stéphane Derosiaux <derste@gmail.com>
2015-12-27 01:54:44 +0100

    Use ES6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-"use strict";
-
-var App = React.createClass({
-  render: function() {
-    var items = this.props.subjects.map(function(subject) {
-      return <li>{subject}</li>;
-    });
+class App extends React.Component {
+  render() {
+    const items = this.props.subjects.map(subject => <li>{subject}</li>)
     return <ul>You like {items}</ul>;
   }
-});
+}
 
 var mountNode = document.getElementById("app");
 ReactDOM.render(<App subjects={[ "Javascript", "Java" ]} />, mountNode);
