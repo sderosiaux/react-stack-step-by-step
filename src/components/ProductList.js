d2f8bd5dad35d86a90a69c2474f182d57eb47c94
chtefi <derste@gmail.com>
2016-01-06 23:58:14 +0100

    Put back some semicolons and commas

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,13 +4,13 @@ import Product from './Product.js';
 const STYLE_UL = {
   display: 'flex',
   flexDirection: 'row',
-  flexWrap: 'wrap'
+  flexWrap: 'wrap',
 };
 
 const STYLE_LI = {
   listStyleType: 'none',
   margin: 10,
-  flex: 1
+  flex: 1,
 };
 
 export default class extends React.Component {
@@ -20,6 +20,6 @@ export default class extends React.Component {
       <ul style={STYLE_UL}>
         {products.map(p => <li style={STYLE_LI}><Product {...p} /></li>)}
       </ul>
-    )
+    );
   }
 }
