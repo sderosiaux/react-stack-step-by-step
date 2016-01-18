8b09ab93d073a3dfce31ac9ccf00ef0705764eac
chtefi <derste@gmail.com>
2016-01-19 00:58:48 +0100

    Extract init parameter

--- src/reducers/boardState.js
+++ src/reducers/boardState.js
@@ -1,6 +1,8 @@
 import { RENAMING, RENAMED } from '../actions/types.js';
 
-export default (state = { name: 'My board', isRenaming: false }, action) =>
+const DEFAULT_BOARD_STATE = { name: 'My board', isRenaming: false };
+
+export default (state = DEFAULT_BOARD_STATE, action) =>
   action.type === RENAMING ? { ...state, isRenaming: true } :
   action.type === RENAMED ? { ...state, isRenaming: false, name: action.newName } :
   state;
